438e13fb9fb74b63cf77a55fff6d088b9b78ce51
Gokul <55547509+gokul-krishna-r@users.noreply.github.com>
2021-12-14 09:17:04 +0530

    Add files via upload

--- cycle_3/prgm_1.cpp
+++ cycle_3/prgm_1.cpp
@@ -19,17 +19,18 @@ class Date{
 			
 			if(year>0 && 0<month<13&& 0<day<32){
 			day+=a;
+			do{
             if(day>31 && (month==1||month==3||month==5||month==7||month==8||month==10||month==12)){
 				day=day-31;
-				if(month=12){
+				if(month==12){
 					year=year+1;
 					month=1;
 				}else{
 					month=month+1;
 				}
 			}else if(day>30 && (month==4||month==6||month==9||month==11)){
-				day=day-31;
-				if(month=12){
+				day=day-30;
+				if(month==12){
 					year=year+1;
 					month=1;
 				}else{
@@ -46,12 +47,13 @@ class Date{
                   month=3;
                    }
             }
-
+		}while(day>31);
+		
 		}else{
 			cout<<"Date is invalid"<<endl;
 			}		
 					
-
+		
 
 		}
 
@@ -71,5 +73,6 @@ int main(){
 	cin>>n;
 	d+n;
 	d.display();
+	
 return 0;
 }
