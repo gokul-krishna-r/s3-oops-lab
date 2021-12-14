91ba38b628d5c1e2a135d12e9e4b06286b4359f2
Gokul <55547509+gokul-krishna-r@users.noreply.github.com>
2021-12-14 10:53:19 +0530

    Add files via upload

---
+++ cycle_3/prgm_3.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+class Complex{
+	float real,img;
+	public:
+		Complex(){
+			real=0;
+			img=0;
+		}
+		Complex (float a,float b){
+			real=a;
+			img=b;
+		}
+		friend ostream& operator <<(ostream& out,Complex &c);
+		friend istream& operator >>(istream& in,Complex &c);
+	
+};
+
+ostream& operator <<(ostream& out,Complex &c){
+			out<<c.real<<" + i"<<c.img<<endl;
+}
+istream& operator >>(istream& in,Complex &c){
+			cout<<"Enter real part: ";
+			in>>c.real;
+			cout<<"Enter imaginary part: ";
+			in>>c.img;
+}
+
+
+
+int main(){
+	Complex c;
+	cin>>c;
+	cout<<c;
+	return 0;
+}
